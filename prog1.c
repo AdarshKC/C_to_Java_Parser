bfc4fc685826acc89efe84caf81d05196e55ca52
Weap0nX <44719874+Weap0nX@users.noreply.github.com>
2020-02-21 14:36:49 +0530

    Updated translation for simple printf()

--- prog1.c
+++ prog1.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
 #include <string.h>
+
+int max(int x, int y)
+{
+	if(x > y)
+	{return x;}
+	return y;
+}
+
 /*
   You can write C code here or look at the examples.
   It will be translated as "demo/demo_translation.c".
-  There are also translations of full p    rograms below.
+  There are also translations of full programs below.
 */
 int main(int argc, char** argv)
 {
+	if(2 >1)
+		{printf("true");}
+	else {printf("false");}
   printf("Number of arguments: ");
-  printf("%d\n", argc);
-  printf("Without program name: ");
-  printf("%d\n", argc - 1);
+  printf("Without programname: ");
   return 0;
 }
