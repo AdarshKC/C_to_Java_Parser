ff77d5f4ef462f553c06545fc15be9d439718236
Adarsh Chaudhary <adarshrohit217@gmail.com>
2020-04-16 21:21:32 +0530

    Did a couple of minor changes

--- mytest.py
+++ mytest.py
@@ -20,15 +20,18 @@ class MyCVisitor(CVisitor):
 
 
 class coverCases():
-    def func(self,adarsh):
-        print("public static " + adarsh, file=result, end=" ")
+    def printWithoutNewLine(self,var):              # print the word without newline
+        print(var, file=result, end=" ")
+
+    def printWithNewLine(self, var):                # print the word with newline
+        print(var, file=result, end="\n")
 
     def spaceGeneration(self,tab):                  # for proper indentations...
         for x in range(tab):
             print("\t", file=result, end="")
 
     def checkIgnore(self,var):
-        if var in ignoreList:  # new line begins after this...
+        if var in ignoreList:                       # write nothing if found this...
             return 1
         return 0
 
@@ -45,111 +48,108 @@ def main(argv):
 
     print("package demo;\n\npublic class DemoTranslation {\n", file=result, end="")  # default format of java program
 
-    tab = 0                                 # for handling indentation
-    beginFlag = 1                           # to check for line beginning
-    argFlag = 0                             # for command line input
-    ignore = 0                              # java translation for command line input
-    funFlag = 0                             # for return type of functions
-    printFlag = 0                           # to handle print statements
-    forFlag = 0                             # to handle for loop
-    c = 0                                   # index of variable list
-    declarationFlag = 0                     # to handle newline in declaration of arrays etc
-    stringLiteral = ""                      # message part in print statements
-    variableList = []                       # variables part in print statements
+    tab = 0                                 # for handling indentation...
+    beginFlag = 1                           # to check for line beginning...
+    argFlag = 0                             # for command line input...
+    ignore = 0                              # java translation for command line input...
+    funFlag = 0                             # for return type of functions...
+    printFlag = 0                           # to handle print statements...
+    forFlag = 0                             # to handle for loop...
+    c = 0                                   # index of variable list...
+    declarationFlag = 0                     # to handle newline in declaration of arrays etc...
+    stringLiteral = ""                      # message part in print statements...
+    variableList = []                       # variables part in print statements...
+
+    solve = coverCases()                    #instance of class to access class functions...
 
     while len(contextList) > 0:             # runs till we found node in the traversal tree...
 
         t = 0
         firstChild = contextList[0]
+        contextList.pop(0)
 
         if str(type(firstChild)) == "<class 'gen.CParser.CParser.FunctionDefinitionContext'>":
             funFlag = 1
-
         if str(type(firstChild)) == "<class 'gen.CParser.CParser.DeclarationContext'>":
             declarationFlag += 1
-
         if firstChild.getText() in ["for"]:
             forFlag += 2
-
-        contextList.pop(0)
-        instance_ = coverCases()
 
         if firstChild.getChildCount() > 0:
             for x in range(0, firstChild.getChildCount()):
                 contextList.insert(t, firstChild.children[x])
                 t = t + 1
         elif firstChild.getChildCount() == 0:   # enters when leaf is found...
+            word = firstChild.getText()
             if beginFlag == 1:
-                instance_.spaceGeneration(tab)
-                if (firstChild.getText() != "}") and beginFlag == 1:
+                solve.spaceGeneration(tab)
+                if (word != "}") and beginFlag == 1:
                     print("\t", file=result, end="")
-            if instance_.checkIgnore(firstChild.getText()) :
+            if solve.checkIgnore(word) :
                 continue
-            elif firstChild.getText() in [";"]:               # new line begins after this...
+            elif word in [";"]:               # new line begins after this...
                 if forFlag == 0:
-                    print(firstChild.getText(), file=result, end="\n")
+                    solve.printWithNewLine(word)
                     beginFlag = 1
                 else:
                     forFlag -= 1
-                    print(firstChild.getText(), file=result, end=" ")
+                    solve.printWithoutNewLine(word)
                 declarationFlag = 0
-            elif firstChild.getText() in ["{"]:             # new line begins after this...
+            elif word in ["{"]:             # new line begins after this...
                 if declarationFlag == 0:
-                    print(firstChild.getText(), file=result, end="\n")
+                    solve.printWithNewLine(word)
                     tab += 1
                     beginFlag = 1
                 else:
-                    print(firstChild.getText(), file=result, end=" ")
-            elif firstChild.getText() in ["printf"]:        # translate print statement
+                    solve.printWithoutNewLine(word)
+            elif word in ["printf"]:        # translate print statement...
                 print("System.out.print", file=result, end="")
                 printFlag = 1
                 beginFlag = 0
-            elif firstChild.getText() in ["int", "void"] and funFlag == 1:  # handle return type of functions
-                print("public static " + firstChild.getText(), file=result, end=" ")
+            elif word in ["int", "void"] and funFlag == 1:  # handle return type of functions...
+                print("public static " + word, file=result, end=" ")
                 beginFlag = 0
                 funFlag = 0
-            elif firstChild.getText() in ["main"]:
-                print(firstChild.getText(), file=result, end=" ")
+            elif word in ["main"]:
+                solve.printWithoutNewLine(word)
                 argFlag = 1
-            elif firstChild.getText() in ["}"]:             # new line begins after this...
+            elif word in ["}"]:             # new line begins after this...
                 if declarationFlag == 0:
-                    print(firstChild.getText(), file=result, end="\n")
+                    solve.printWithNewLine(word)
                     tab -= 1
                     beginFlag = 1
                 else:
-                    print(firstChild.getText(), file=result, end=" ")
-            elif firstChild.getText() in ["("] and argFlag == 1:
-                print(firstChild.getText(), file=result, end=" ")
+                    print(word, file=result, end=" ")
+            elif word in ["("] and argFlag == 1:
+                solve.printWithoutNewLine(word)
                 ignore = 1
-            elif firstChild.getText() in ["("]:
-                print(firstChild.getText(), file=result, end=" ")
+            elif word in ["("]:
+                solve.printWithoutNewLine(word)
                 c = -1
                 if printFlag == 1:
                     printFlag = 2
-            # elif firstChild.getText() in [")"]:
-            #     printFlag = 0
-            elif firstChild.getText() in [")"] and argFlag == 1:
+            elif word in [")"] and argFlag == 1:
                 print("String[] args )", file=result, end=" ")
                 ignore = 0
                 argFlag = 0
             else:                                       # translation of print arguments and messages(differently)...
-                if printFlag == 2:                      # store message of print statement
-                    stringLiteral = firstChild.getText()
+                if printFlag == 2:                      # store message of print statement...
+                    stringLiteral = word
                     printFlag = 3
-                elif printFlag == 3:                    # variable part of print statement
+                elif printFlag == 3:                    # variable part of print statement...
                     temp1 = 0
-                    if firstChild.getText() in [")"]:
-                        for var in stringLiteral:       # look for data type in message part
+                    if word in [")"]:
+                        for var in stringLiteral:       # look for data type in message part...
                             if temp1 == 1:
                                 if var in dataList:     # if data type is found
                                     print("\");\n", file=result, end="")
-                                    instance_.spaceGeneration(tab+1) # for proper indentations...
+                                    solve.spaceGeneration(tab+1) # for proper indentations...
                                     print("System.out.print(", file=result, end=" ")
                                     if variableList[0] in ["argc"]:     # command line input translation...
                                         print("args.length+1 ) ;\n", file=result, end="")
                                     else:
                                         print(variableList[0] + " );\n", file=result, end="")
-                                    instance_.spaceGeneration(tab+1) # for proper indentations...
+                                    solve.spaceGeneration(tab+1) # for proper indentations...
                                     print("System.out.print( \"", file=result, end="")
                                     variableList.pop(0)
                                 else:
@@ -163,14 +163,14 @@ def main(argv):
                         c = -1
                         printFlag = 0
                         print(")", file=result, end="")
-                    if firstChild.getText() not in [",", ")"]:
-                        variableList.insert(c, variableList[c]+firstChild.getText())  # handle expression in print...
+                    if word not in [",", ")"]:
+                        variableList.insert(c, variableList[c]+word)  # handle expression in print...
                     else:
                         variableList.append("")
                         c += 1
                 else:
                     if ignore == 0:
-                        print(firstChild.getText(), file=result, end=" ")              # print rest of the program...
+                        solve.printWithoutNewLine(word)
                         beginFlag = 0
 
     print("}", file=result, end="")
