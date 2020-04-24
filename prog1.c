e64a64b1ae24b22855d2cb3ac32ea577fed2a367
Weap0nX <44719874+Weap0nX@users.noreply.github.com>
2020-04-24 11:07:14 +0530

    Updated input file

--- prog1.c
+++ prog1.c
@@ -1,27 +1,52 @@
-int max(int x, int y)
-{
-	if(x > y)
-	return y;
+#include<stdio.h>
 
-	return x;
+int search(int arr[], unsigned int arr_len, int x){  
+    for (unsigned int i = 0; i < arr_len; i++) 
+	if (arr[(unsigned int)i] == x) 
+	    return (int)i; 
+    return -1; 
+} 
+
+int factorial(int num);
+
+int verifyFactorial(int num){
+    int fact = 1;
+    if(num < 0){
+    	fact = -1;
+    }
+    else if(num > 1){ 
+	    do{
+		fact = fact * num;
+		num = num - 1;
+	     }while(num>1);
+    }
+    else {
+    	fact = 1;
+    }
+    return fact;	
 }
 
-/*
-  You can write C code here or look at the examples.
-  It will be translated as "demo/demo_translation.c".
-  There are also translations of full programs below.
-*/
-void main(int argc, char** argv)
-{
-	int i = 2, j = 2, k =2;
-	if(2 >1)
-		{printf("true");}
-	else {printf("false");}
-	while (j>i)
-	{j=j-1;
-	i=i+1;}
-	printf("Value of this %d And done.\n", i+j+k);
-	printf("Value of argument %d And done.\n", argc);
-	printf("Value of this %d And done.\n", i+j+k);
-
+void main() {
+	   int arr[] = { 1, 2, 3, 4, 5, 6 }; 
+	   int num = 6; 
+	   int result = search(arr, 6, num); 
+	   printf("%d\n", result); 
+	   result = factorial(num);
+	   printf("%d\n", result); 
 }
+
+//This should be linked to its function declaration 
+int factorial(int num){
+    int copyNum = num;
+    int fact = 1;
+    if(num < 0)
+    	fact = -1;
+    else{
+	while(num > 1){
+	    fact = fact * num--;
+	}
+    }
+    return fact==verifyFactorial(copyNum)? fact:-2;
+}
+
+
